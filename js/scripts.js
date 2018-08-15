52bc64763fd19d8a420f472389694fc05482ebac
Grace Adamiec and William Wittenbrock <william@>
2018-08-15 16:42:17 -0700

    Add javascript logic that can determine if an inputted sentence begins with a vowel or not.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
 // BACKEND LOGIC
 
+const vowels = ["a", "e", "i", "o", "u"];
+const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+
+// var pigLatin = function(userSentence) {
+//   if () {
+//
+//   } else {
+//     return false;
+//   }
+//
+// }
+
+// Given a word, returns true if the first letter is a vowel, returns false otherwise.
+var isAVowel = function(userString, letterPosition) {
+  const vowels = /(a|e|i|o|u)/i;
+  var letter = userString.charAt(letterPosition);
+  var test = vowels.exec(letter);
+  if (test !== null) {
+    return true
+  } else {
+    return false
+  }
+}
+
 
 
 
@@ -11,10 +35,16 @@ $(document).ready(function(){
     event.preventDefault();
 
     var userSentence = $("input#userSentence").val();
-    var words = stringToWordsArray(userSentence);
+    var sentenceBeginWithAVowel = function() {
+      if (isAVowel(userSentence, 0) === true) {
+        return "Your sentence starts with a vowel! Way to go!"
+      } else {
+        return "That sentence does not start with a vowel. Better luck next time."
+      }
+    };
 
 
-   $("#result").text(result);
+   $("#result").text(sentenceBeginWithAVowel);
 
 
 
